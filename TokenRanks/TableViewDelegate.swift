7c39a81c816384cef0120cf953174904cc23bc4b
Gelei Chen <chen1474@purdue.edu>
2017-09-15 14:11:31 -0400

    init

---
+++ TokenRanks/TableViewDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewDelegate.swift
+//  TokenRanks
+//
+//  Created by Gelei Chen on 14/9/2017.
+//  Copyright © 2017 Gelei. All rights reserved.
+//
+
+import UIKit
+extension ViewController: UITableViewDelegate,UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+//        cell.textLabel?.text = "123"
+        return cell
+    }
+}
--- TokenRanks/ViewController.swift
+++ TokenRanks/ViewController.swift
@@ -7,11 +7,21 @@
 //
 
 import UIKit
+import Fabric
+import Crashlytics
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        Fabric.with([Crashlytics.self])
+        self.navigationController?.navigationBar.tintColor = UIColor.black
+        self.navigationController?.navigationBar.barTintColor = UIColor.white
+        self.navigationController?.navigationBar.isTranslucent = false
+        self.tabBarController?.tabBar.barTintColor = UIColor.black
+        self.tabBarController?.tabBar.tintColor = UIColor.white
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: ">", style: UIBarButtonItemStyle.plain, target: self, action: #selector(ViewController.dateSelected))
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "<", style: UIBarButtonItemStyle.plain, target: self, action: #selector(ViewController.dateSelected))
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +30,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func dateSelected(){
+        self.title = "Sep 13"
+    }
 
 }
 
